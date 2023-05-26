ba383224195cfbe0fb5b19ac14644e790e3659bb
koya <kouyan120706@gmail.com>
2023-05-26 21:41:16 +0900

    Added AWS Inter-Region RTT Measurement Script

---
+++ rtt-measure/rtt_exp.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#set -x
+
+hosts_list="config.txt"
+
+# use UTC throughout the experiment
+export TZ=UTC
+
+# results will be stored here
+base_dir=~/aws-tools/data/aws-inter-region-rtt
+
+ssh_option="-i ~/.ssh/id_rsa -y"
+
+#regions=(ohio virginia california oregon mumbai seoul singapore sydney tokyo canada frankfurt ireland london saopaulo)
+regions=($(cut -d " " -f 1 "$hosts_list"))
+region_prefix=bft-
+
+user=ubuntu
+
+server_command=ping
+server_option="-c 10"
+
+now() {
+	echo -n "$(date +%s) $(date +'%F %H:%M:%S %:z')"
+}
+
+mkdir -p "$base_dir"
+output_dir="$base_dir/$(date +'%Y%m%d-%H%M')"
+mkdir -p "$output_dir"
+
+# start time
+echo "> Experiments-Start: $(now)"
+
+for i in "${regions[@]}"
+do
+	server_ip=$(grep "$i" "$hosts_list" | cut -d ' ' -f 2)
+	mkdir -p "$output_dir/$i"
+
+	echo ">> $i-Region-Start: $(now)"
+
+	for j in "${regions[@]}"
+	do
+		client_ip=$(grep "$j" "$hosts_list" | cut -d ' ' -f 2)
+		output="$output_dir/$i/$j.txt"
+
+		echo ">>> $i-$j: $(now)"
+
+		# start client
+		ssh -oStrictHostKeyChecking=no $ssh_option "$server_ip" -l $user $server_command $server_option "$client_ip" > "$output" &
+	done
+
+	echo ">> $i-Region-End: $(now)"
+done
+
+# finish time
+echo "> Experiments-Finish: $(now)"
