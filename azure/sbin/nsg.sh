caaa34c67624bcfef7c783adaab1566e9730061f
koya <kouyan120706@gmail.com>
2023-05-27 10:51:26 +0900

    Update nsg.sh

--- azure/sbin/nsg.sh
+++ azure/sbin/nsg.sh
@@ -5,4 +5,10 @@ source $sbin/common.sh
 
 load_setting $1
 
-az network nsg rule create --resource-group ${resource_group_name} --nsg-name allow_icmp --name Allow_ICMP --protocol ICMP --direction Inbound --priority 100 --source-address-prefix '*' --destination-address-prefix '*' --access Allow
+
+nsg_names=$(az network nsg list --resource-group ${resource_group_name} --query "[].name" --output tsv)
+
+for nsg_name in $nsg_names; do
+    echo "Adding rule to NSG: $nsg_name"
+    az network nsg rule create --resource-group ${resource_group_name} --nsg-name $nsg_name --name Allow_ICMP --protocol ICMP --direction Inbound --priority 100 --source-address-prefix '*' --destination-address-prefix '*' --access Allow
+done
