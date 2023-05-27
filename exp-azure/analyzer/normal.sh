60f0145ccbc5ead9d9284ede4fc8ddcac6d32060
koya <kouyan120706@gmail.com>
2023-05-27 20:55:04 +0900

    Updated script to calculate standard deviation
    Modified the script to include the calculation of standard deviation for each experiment data set. The standard deviation provides a measure of the dispersion or variability within each data set. This update enhances the analysis by considering the spread of values in addition to the mean. The standard deviation values can be used to assess the consistency and variability of the experimental results. The results with their respective standard deviations are now recorded in the output file for further analysis.

--- exp-azure/analyzer/normal.sh
+++ exp-azure/analyzer/normal.sh
@@ -6,16 +6,21 @@ do
     touch latency/$protocol.txt
     cat exp-list.config | while read LINE
     do
-        #grep -r -w "[0-1] , [0-1] , [0-1] , [0-9]\+ , [0-9]\+"
         exp_id=`echo $LINE | cut -d " " -f 1`
-        data=`cat latency/$exp_id/$protocol/client* | grep -w "[0-1] , 1 , 1 , [0-9]\+ , [0-9]\+"`
+        data=`cat latency/$exp_id/$protocol/client* | grep -w "[0-1] , 1 , [0-1] , [0-9]\+ , [0-9]\+"`
         num=`echo "$data" | wc -l`
         echo -n $LINE 
         echo -n " "
         if [ "$num" == "0" ] || [ "$num" == "1" ]; then
             echo "error"
         else 
-            echo "$data" | sed -e "s/ , /,/g" | cut -d "," -f 4 | sort -n | awk 'BEGIN{OFMT="%d"} { a[i++]=$1; } END { x=int((i+1)/2); if (x < (i+1)/2) print (a[x-1]+a[x])/2; else print a[x-1]; }'
+            # Calculate mean
+            mean=$(echo "$data" | sed -e "s/ , /,/g" | cut -d "," -f 4 | awk '{ sum += $1 } END { printf "%.2f", sum/NR }')
+
+            # Calculate standard deviation
+            std_dev=$(echo "$data" | sed -e "s/ , /,/g" | cut -d "," -f 4 | awk -v mean="$mean" '{ sum += ($1 - mean)^2 } END { printf "%.2f", sqrt(sum/NR) }')
+
+            echo "$mean $std_dev"
         fi
     done > latency/$protocol.txt
 done
