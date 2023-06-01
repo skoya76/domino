caafb4b27c6b328edd1786e3e780a2bf3cff9629
koya <kouyan120706@gmail.com>
2023-06-02 02:17:31 +0900

    Update commit_exec_latency.sh

--- exp-azure/analyzer/commit_exec_latency.sh
+++ exp-azure/analyzer/commit_exec_latency.sh
@@ -7,23 +7,19 @@ do
     cat exp-list.config | while read LINE
     do
         exp_id=`echo $LINE | cut -d " " -f 1`
-        data=`cat latency/$exp_id/$protocol/server-* | grep -a --binary-files=text -E '[[:digit:]]+ ns'`
+        data=`cat latency/$exp_id/$protocol/server* | grep -a --binary-files=text -E '[[:digit:]]+ ns'`
         num=`echo "$data" | wc -l`
         echo -n $LINE 
         echo -n " "
         if [ "$num" == "0" ] || [ "$num" == "1" ]; then
             echo "error"
         else 
-            # 計算結果を配列に格納
-            arr=($(echo "$data" | rev | cut -d " " -f 2 | rev))
-            
             # 平均値の計算
-            sum=$(IFS=+; echo "${arr[*]}")
-            average=$(awk -v sum="$sum" -v num="$num" 'BEGIN {printf "%.0f", sum/num}')
+            average=$(echo "$data" | rev | cut -d " " -f 2 | rev | awk '{sum+=$1} END {printf "%.0f", sum/NR}')
 
             # 標準偏差の計算
-            variance=$(awk -v sum="$sum" -v num="$num" 'BEGIN {s=0; for (i in ARGV) s+=ARGV[i]; avg=s/num; for (i in ARGV) {diff=ARGV[i]-avg; sdiff+=diff*diff} printf "%.0f", sdiff/num}' "${arr[@]}")
-            stddev=$(awk -v variance="$variance" 'BEGIN {printf "%.0f", sqrt(variance)}')
+            variance=$(echo "$data" | rev | cut -d " " -f 2 | rev | awk -v avg="$average" '{sum+=($1-avg)^2} END {printf "%.0f", sum/NR}')
+            stddev=$(echo "$variance" | awk '{printf "%.0f", sqrt($0)}')
 
             echo -n "$average "
             echo -n "$stddev "
